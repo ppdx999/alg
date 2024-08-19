99c82e32b87baa7bb951fe415cba95712d0771f3
fujis <ppdx999@gmail.com>
2024-08-19 14:58:31 +0900

    solve e

--- atcoder/adt_medium_20240815_3/e/main.cpp
+++ atcoder/adt_medium_20240815_3/e/main.cpp
@@ -1,7 +1,30 @@
-#include <stdio.h>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
 
 int main() {
-  vector<int> v;
+  int q;
+  cin >> q;
+  multiset<int> s;
+
+  rep(i,q) {
+    int type;
+    cin >> type;
+    if (type == 1) {
+      int x;
+      cin >> x;
+      s.insert(x);
+    } else if (type == 2) {
+      int x,c;
+      cin >> x >> c;
+      rep(i,c) {
+        auto it = s.find(x);
+        if(it == s.end()) break;
+        s.erase(it);
+      }
+    } else {
+      int ans = *s.rbegin() - *s.begin();
+      cout << ans << endl;
+    }
+  }
 }
